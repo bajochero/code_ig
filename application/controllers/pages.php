04f3284a450db5299882ab1a4cd404f06184af11
bajochero <bajochero@gmail.com>
2014-12-19 10:35:31 +0700

    comit pertama

---
+++ application/controllers/pages.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Controllername extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+	
+
+}
+
+/* End of file controllername.php */
+/* Location: ./application/controllers/controllername.php */
